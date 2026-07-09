a3f1be5e2d023f0e9f7668089ca6ddd25bfd3139
ohishi_MSI2023 <gpu.kenji.ohishi@gmail.com>
2026-07-09 13:21:28 +0900

    7/9 13:21

--- Assets/GPU_RETURNS/Scripts/AnimDemo.cs
+++ Assets/GPU_RETURNS/Scripts/AnimDemo.cs
@@ -3,39 +3,52 @@ using UnityEngine.InputSystem;
 
 public class AnimDemo : MonoBehaviour
 {
-    private Rigidbody2D rb; // リジッドボディ2Dのコンポーネントを保存する変数
-    float moveSpeed;        // 移動速度
+    private Rigidbody2D rb;			// リジッドボディ2Dのコンポーネントを保存する変数
+    float moveSpeed;                // 移動速度
+	Transform visual;				// 子オブジェクトのVisualを保存
+	Vector3 startScale;				// 最初のスケール値
+	InputAction moveAction;			// アクションマップ【Move】を取得
+	InputAction jumpAction;         // アクションマップ【Jump】を取得
 
-    void Start()
+	void Start()
     {
         rb = GetComponent<Rigidbody2D>();       // リジッドボディ2Dコンポーネントを取得
         moveSpeed = 3.0f;                       // 移動速度
-    }
+
+		// アクションマップを取得
+		moveAction = InputSystem.actions.FindAction("Move");
+		jumpAction = InputSystem.actions.FindAction("Jump");
+
+		// Visualの最初のスケール値を保存
+		visual = transform.GetChild(0);
+		startScale = visual.localScale;
+	}
 
     void Update()
     {
-        // Dキーが押されている間
-        if (Keyboard.current.dKey.isPressed)
-        {
-            // 右方向に移動
-            rb.linearVelocity = transform.right * moveSpeed;
-        }
+		// 左右移動
+		Vector3 vel = rb.linearVelocity;
+		vel.x = moveAction.ReadValue<Vector2>().x * moveSpeed;
+		rb.linearVelocity = vel;
 
-        // Aキーが押されている間
-        if (Keyboard.current.aKey.isPressed)
-        {
-            // 左方向へ移動
-            rb.linearVelocity = -transform.right * moveSpeed;
-        }
+		// 画像左右反転
+		Vector3 scale = visual.localScale;
+		if(vel.x < 0f)
+		{
+			scale.x = -startScale.x;
+		}
+		else if(vel.x > 0f)
+		{
+			scale.x = startScale.x;
+		}
+		visual.localScale = scale;
 
-        // .isPressed
-        // 押している間 GetKey
+		// ジャンプ処理
+		if (jumpAction.WasPerformedThisFrame())
+		{
+			rb.AddForce(Vector2.up * 500);
+		}
 
-        // .wasPressedThisFrame
-        // 押した瞬間、GetKeyDown
 
-        // .wasReleasedThisFrame
-        // 離した瞬間、GetKeyUp
-
-    }
+	}
 }
